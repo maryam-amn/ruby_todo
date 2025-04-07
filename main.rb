b65fbc1acbc457123107affc703c75258196c8ae
maryam-amn <Maryam.Aman@jobtrek.ch>
2025-04-07 08:21:58 +0200

    BREAKING CHANGE : all the todos are stored in a Json file

--- main.rb
+++ main.rb
@@ -1,4 +1,13 @@
-my_list = []
+require 'json'
+
+filename = 'todo.json'
+
+begin
+  my_list = JSON.parse(File.read(filename))
+rescue
+  my_list = []
+end
+
 puts "What would you like to do
  (a-> to add a new list):
  (r-> to remove a list:)  "
@@ -17,12 +26,15 @@ when "a"
   end
   puts "Your to do list  : ", my_list
 
+  File.open(filename, 'w') do |f|
+    f.write(JSON.pretty_generate(my_list))
+  end
+
 when "r"
   puts "Ajouter supprimé."
 
 else
   puts "Commande inconnue."
 end
-
 
 
